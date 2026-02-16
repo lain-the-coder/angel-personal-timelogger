5a4c5d0f650008918f2b6984c90e91ed6ce7170b
Ehab Ahmed <ahmed.ehabj@gmail.com>
2026-02-16 20:40:38 +0400

    Minor structural updates to Phase 1

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+//Declared variables
 let currentTask = null;
 const start = document.getElementById("start-btn");
 const taskName = document.getElementById("task-name");
 const taskNotes = document.getElementById("task-notes");
 const timerForm = document.getElementById("timer-form");
 const timerDisplay = document.getElementById("timer-display");
+const currentTracking = document.getElementById("current-task-name");
 
 function StartTracking() {
   //Form validation for required field
@@ -11,17 +13,22 @@ function StartTracking() {
     alert("Please enter a task name");
     return;
   }
+
+  //Save field values and start time into an object
   currentTask = {
     task: taskName.value,
     notes: taskNotes.value,
     startTime: new Date(),
   };
-  const currentTracking = document.getElementById("current-task-name");
+
+  //To display the current tracking field on UI
   currentTracking.textContent = currentTask.task;
+
   //block switch to timer upon click of Start Tracking button
   timerDisplay.style.display = "block";
   timerForm.style.display = "none";
 
+  //Console statements
   console.log("Start Tracking button clicked");
   console.log(currentTask);
 }
