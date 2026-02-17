f0c8a1a5827eae83d796d165a43386ce37d118c7
Ehab Ahmed <ahmed.ehabj@gmail.com>
2026-02-17 15:24:04 +0400

    Phase 3: Stop & Save ✓ Stop button handler with clearInterval ✓ Duration calculation in seconds (endTime - startTime) ✓ Save completed tasks to localStorage with JSON ✓ Form reset and UI state management ✓ Multiple tasks persist across sessions

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 //Declared variables
 let currentTask = null;
 let timerInterval = null;
+let completedTask = null;
 const start = document.getElementById("start-btn");
+const stop = document.getElementById("stop-btn");
 const taskName = document.getElementById("task-name");
 const taskNotes = document.getElementById("task-notes");
 const timerForm = document.getElementById("timer-form");
@@ -68,3 +70,62 @@ function StartTracking() {
 
 //Event Listener for Start Tracking click button
 start.addEventListener("click", StartTracking);
+
+function StopTracking() {
+  //Stop timer after clicking Stop Tracking button
+  clearInterval(timerInterval);
+
+  //Capture endtime and duration in seconds along with round off
+  const endTime = new Date();
+  const durationInSeconds = Math.floor(
+    (endTime.getTime() - currentTask.startTime.getTime()) / 1000,
+  );
+
+  //Store final task data in an object
+  completedTask = {
+    taskName: currentTask.task,
+    notes: currentTask.notes,
+    startTime: currentTask.startTime,
+    endTime: endTime,
+    duration: durationInSeconds,
+  };
+
+  //Retrieve existing data from localstorage
+  const existingTasks = localStorage.getItem("tasks");
+
+  let savedTasks = [];
+
+  //Check if key already exists
+  if (existingTasks === null) {
+    savedTasks = [];
+  } else {
+    savedTasks = JSON.parse(existingTasks);
+  }
+
+  //Push the new object into the array
+  savedTasks.push(completedTask);
+
+  //Stringify the updated array  back into a JSON string
+  const savedTasksString = JSON.stringify(savedTasks);
+
+  //Save the updated string to localstorage
+  localStorage.setItem("tasks", savedTasksString);
+
+  //Hide timer display and display form
+  timerDisplay.style.display = "none";
+  timerForm.style.display = "block";
+
+  //Clear form values
+  taskName.value = "";
+  taskNotes.value = "";
+
+  //Reset current task to null
+  currentTask = null;
+
+  //Console statements
+  console.log(completedTask);
+  console.log("Stop button clicked");
+}
+
+//Event Listener for Stop Tracking click button
+stop.addEventListener("click", StopTracking);
